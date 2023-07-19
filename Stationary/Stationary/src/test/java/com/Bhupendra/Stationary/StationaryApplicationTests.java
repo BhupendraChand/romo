906acceb571bfe71c6c44621f9c4944dbe6f31e1
BhupendraChand <133785701+BhupendraChand@users.noreply.github.com>
2023-07-19 12:47:59 +0545

    Add files via upload

---
+++ Stationary/Stationary/src/test/java/com/Bhupendra/Stationary/StationaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bhupendra.Stationary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StationaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
